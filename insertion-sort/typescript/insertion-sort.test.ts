53bcbb6c23eaba8312c2b01d9ae519414837abb5
nixpig <143995476+nixpig@users.noreply.github.com>
2023-09-28 06:06:26 +0100

    chore: update ts test data to be same

--- insertion-sort/typescript/insertion-sort.test.ts
+++ insertion-sort/typescript/insertion-sort.test.ts
@@ -1,8 +1,8 @@
 import { insertionSort } from "./insertion-sort";
-const unsorted = [5, 2, 4, 6, 1, 3];
+const unsorted = [23, 13, 7, 69, 42];
 
 describe("insertion sort", () => {
   test("should sort an array of numbers", () => {
-    expect(insertionSort<number>(unsorted)).toEqual([1, 2, 3, 4, 5, 6]);
+    expect(insertionSort<number>(unsorted)).toEqual([7, 13, 23, 42, 69]);
   });
 });
