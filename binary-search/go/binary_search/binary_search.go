59babf46767a15be3bd3ab9de626f410cab68b5f
nixpig <143995476+nixpig@users.noreply.github.com>
2023-09-29 07:49:41 +0100

    feat(go): implement binary search

---
+++ binary-search/go/binary_search/binary_search.go
@@ -0,0 +1,32 @@
+package binary_search
+
+import (
+	"math"
+)
+
+func main() {
+
+}
+
+func BinarySearch(arr []int, target int) int {
+	indexHigh := len(arr) - 1
+	indexLow := 0
+	var indexMid int
+
+	for indexLow < indexHigh {
+		indexMid = int(math.Floor(float64(indexHigh+indexMid) / 2))
+
+		if arr[indexMid] == target {
+			return indexMid
+		}
+
+		if arr[indexMid] > target {
+			indexHigh = indexMid - 1
+		} else {
+
+			indexLow = indexMid + 1
+		}
+	}
+
+	return -1
+}
---
+++ binary-search/go/binary_search/binary_search_test.go
@@ -0,0 +1,58 @@
+package binary_search
+
+import "testing"
+
+func TestBinarySearch_UpperHalf(t *testing.T) {
+	arr := []int{1, 7, 23, 42, 75, 116, 794, 800, 1099}
+
+	got := BinarySearch(arr, 800)
+	want := 7
+
+	if got != want {
+		t.Errorf("got %q, wanted %q", got, want)
+	}
+}
+
+func TestBinarySearch_LowerHalf(t *testing.T) {
+	arr := []int{1, 7, 23, 42, 75, 116, 794, 800, 1099}
+
+	got := BinarySearch(arr, 23)
+	want := 2
+
+	if got != want {
+		t.Errorf("got %q, wanted %q", got, want)
+	}
+}
+
+func TestBinarySearch_MidPoint(t *testing.T) {
+	arr := []int{1, 7, 23, 42, 75, 116, 794, 800, 1099}
+
+	got := BinarySearch(arr, 75)
+	want := 4
+
+	if got != want {
+		t.Errorf("got %q, wanted %q", got, want)
+	}
+}
+
+func TestBinarySearch_OutOfBounds_Upper(t *testing.T) {
+	arr := []int{1, 7, 23, 42, 75, 116, 794, 800, 1099}
+
+	got := BinarySearch(arr, 1200)
+	want := -1
+
+	if got != want {
+		t.Errorf("got %q, wanted %q", got, want)
+	}
+}
+
+func TestBinarySearch_OutOfBounds_Lower(t *testing.T) {
+	arr := []int{1, 7, 23, 42, 75, 116, 794, 800, 1099}
+
+	got := BinarySearch(arr, -23)
+	want := -1
+
+	if got != want {
+		t.Errorf("got %q, wanted %q", got, want)
+	}
+}
