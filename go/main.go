b6d8d5bb8e673dea8da1ae2a9746f652839eae71
nixpig <143995476+nixpig@users.noreply.github.com>
2023-10-17 07:48:28 +0100

    chore(go): check that generic max heap works

--- go/main.go
+++ go/main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	heap := max_heap.MaxHeap[string]{}
+	stringHeap := max_heap.MaxHeap[string]{}
 
 	values := []max_heap.HeapNode[string]{
 		*max_heap.NewHeapNode(23, "foo"),
@@ -21,16 +21,40 @@ func main() {
 	}
 
 	for _, value := range values {
-		heap.Insert(value)
+		stringHeap.Insert(value)
 	}
 
-	for heap.GetLength() > 0 {
-		max := heap.RemoveMax()
+	for stringHeap.GetLength() > 0 {
+		max := stringHeap.RemoveMax()
 		if max != nil {
 			fmt.Println("max:", *max)
 		} else {
 			fmt.Println("no items left in heap")
 		}
 
+	}
+
+	mapHeap := max_heap.MaxHeap[map[int]string]{}
+
+	items := []max_heap.HeapNode[map[int]string]{
+		*max_heap.NewHeapNode(14, map[int]string{23: "foo"}),
+		*max_heap.NewHeapNode(34, map[int]string{42: "apple"}),
+		*max_heap.NewHeapNode(73, map[int]string{12223: "banana"}),
+		*max_heap.NewHeapNode(900, map[int]string{834: "dog"}),
+		*max_heap.NewHeapNode(4, map[int]string{1: "cat"}),
+		*max_heap.NewHeapNode(19, map[int]string{0: "tiger"}),
+	}
+
+	for _, item := range items {
+		mapHeap.Insert(item)
+	}
+
+	for mapHeap.GetLength() > 0 {
+		got := mapHeap.RemoveMax()
+		if got != nil {
+			fmt.Println("got:", got)
+		} else {
+			fmt.Println("no items left")
+		}
 	}
 }
